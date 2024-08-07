89fd98dcfdf2229bb85bd29292aa4eddae3419fa
Till Tomczak <96786986+tilltmk@users.noreply.github.com>
2024-08-07 14:35:24 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, url_for
+import subprocess
+import os
+
+app = Flask(__name__)
+
+# Funktion zum Abrufen der bestehenden Benutzer
+def get_users():
+    try:
+        output = subprocess.check_output("cat /etc/openvpn/easy-rsa/pki/index.txt | grep -E 'V\t'", shell=True)
+        users = [line.split('\t')[-1] for line in output.decode('utf-8').split('\n') if line]
+        return users
+    except Exception as e:
+        return str(e)
+
+# Funktion zum Hinzufügen eines neuen Benutzers
+def add_user(username):
+    try:
+        subprocess.check_call(f"cd /etc/openvpn/easy-rsa && ./easyrsa build-client-full {username} nopass", shell=True)
+        return True
+    except Exception as e:
+        return str(e)
+
+# Funktion zum Abrufen des Serverstatus
+def get_server_status():
+    try:
+        output = subprocess.check_output("systemctl status openvpn@server", shell=True)
+        return output.decode('utf-8')
+    except Exception as e:
+        return str(e)
+
+# Funktion zum Abrufen der Serverauslastung
+def get_server_load():
+    try:
+        output = subprocess.check_output("uptime", shell=True)
+        return output.decode('utf-8')
+    except Exception as e:
+        return str(e)
+
+# Funktion zum Abrufen des Traffics
+def get_traffic():
+    try:
+        output = subprocess.check_output("vnstat -i tun0", shell=True)
+        return output.decode('utf-8')
+    except Exception as e:
+        return str(e)
+
+# Funktion zum Abrufen der Konfiguration
+def get_configuration():
+    try:
+        with open('/etc/openvpn/server.conf', 'r') as file:
+            config = file.read()
+        return config
+    except Exception as e:
+        return str(e)
+
+@app.route('/')
+def index():
+    users = get_users()
+    server_status = get_server_status()
+    server_load = get_server_load()
+    traffic = get_traffic()
+    config = get_configuration()
+    return render_template('index.html', users=users, server_status=server_status, server_load=server_load, traffic=traffic, config=config)
+
+@app.route('/add_user', methods=['POST'])
+def add_user_route():
+    username = request.form['username']
+    result = add_user(username)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(port=900, debug=True)
